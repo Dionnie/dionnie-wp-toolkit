ee082ca525b1d74690a509326bc9070966d9bba6
Mark Dionnie Bulingit <Dionnie>
2026-07-21 20:33:58 +0800

    =chore: remove WIP modules to clean base boilerplate

--- includes/Plugin.php
+++ includes/Plugin.php
@@ -2,16 +2,7 @@
 
 namespace DionnieBoilerplatePlugin;
 
-define('DIONNIE_BOILERPLATE_PLUGIN_NAME', 'DionnieBoilerplatePlugin');
-define('DIONNIE_BOILERPLATE_PLUGIN_VERSION', '1.0.0');
-define('DIONNIE_BOILERPLATE_PLUGIN_SLUG', 'dionnie-boilerplate-plugin');
-define('DIONNIE_BOILERPLATE_PLUGIN_PATH', plugin_dir_path(__FILE__));
-define('DIONNIE_BOILERPLATE_PLUGIN_URL', plugin_dir_url(__FILE__));
-define('DIONNIE_BOILERPLATE_PLUGIN_FILE', __FILE__);
 
-if (!defined('DIONNIE_BOILERPLATE_PLUGIN_DEV_MODE')) {
-    define('DIONNIE_BOILERPLATE_PLUGIN_DEV_MODE', file_exists(DIONNIE_BOILERPLATE_PLUGIN_PATH . 'public/hot'));
-}
 
 use DionnieBoilerplatePlugin\Registerable;
 use DionnieBoilerplatePlugin\Helpers\DependencyChecker;
